a2f555247e0839649605515b2ed6a28a9926ead1
eisenhaus335 <greenzone332@protonmail.com>
2023-08-14 05:20:23 +0000

    wrong folder

--- sanic_htmx/sanic_htmx/extensions.py
+++
@@ -1,8 +0,0 @@
-from sanic_ext import Extension, Extend
-
-from .base import HtmxContext
-class HtmxExtension(Extension):
-    name = "htmx"
-
-    def startup(self, bootstrap: Extend):
-        bootstrap.add_dependency(HtmxContext, HtmxContext.parse_htmx)
